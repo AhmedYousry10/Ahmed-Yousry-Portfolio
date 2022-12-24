b4ee745b7839cc0913a8c7ff85d4a8c3cd597800
Ahmed Yousry Helal <102818092+AhmedYousry10@users.noreply.github.com>
2022-12-24 17:46:07 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Typing Animation code 
+var typed = new Typed(".typing",{
+  strings:["Web Developer", "Web Designer", "Civil Engineer"],
+  typeSpeed:100,
+  backSpeed:60,
+  loop:true
+})
+
+
+
